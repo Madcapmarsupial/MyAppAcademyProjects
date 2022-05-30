a670682f5113cc7017f6dd480e2ebdcd6d9d4766
Numbat <samueltylergreen@gmail.com>
2022-05-29 17:05:41 -0700

    Handles errors

--- w4d7/error_handling/super_useful.rb
+++ w4d7/error_handling/super_useful.rb
@@ -5,12 +5,14 @@ end
 
 # PHASE 3
 FRUITS = ["apple", "banana", "orange"]
-
+COFFEES = ["latte", "machiatto", "doppio"]
 def reaction(maybe_fruit)
   if FRUITS.include? maybe_fruit
     puts "OMG, thanks so much for the #{maybe_fruit}!"
-  else 
-    raise StandardError 
+  elsif  COFFEES.include? maybe_fruit
+    raise ArgumentError.new "hey!, that's not fruit!"
+  else
+    raise StandardError
   end 
 end
 
@@ -18,16 +20,31 @@ def feed_me_a_fruit
   puts "Hello, I am a friendly monster. :)"
 
   puts "Feed me a fruit! (Enter the name of a fruit:)"
-  maybe_fruit = gets.chomp
-  reaction(maybe_fruit) 
+  begin 
+    maybe_fruit = gets.chomp
+    reaction(maybe_fruit) 
+  rescue ArgumentError
+    puts "I do like coffee, but feed me fruit!"
+    retry
+  end
 end  
 
 # PHASE 4
 class BestFriend
   def initialize(name, yrs_known, fav_pastime)
+
+  ensure 
+    e = "E"
+    e += ", name must not be blank" if name.length < 1
+    e += ", years_known must be greater then 4" if yrs_known < 5
+    e += ", past_time must not be blank" if fav_pastime.length < 1
+    raise ArgumentError.new(e) if e != "E"
+
     @name = name
+    #raise ArgumentError.new "years_known must be 5 or greater" if yrs_known < 5
     @yrs_known = yrs_known
     @fav_pastime = fav_pastime
+
   end
 
   def talk_about_friendship
