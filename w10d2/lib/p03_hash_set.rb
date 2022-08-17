374f303796782bcf443a023bfde956237abc6343
yippy <samueltylergreen@gmail.com>
2022-08-17 14:03:52 -0700

    fills out hash set solution

--- w10d2/lib/p03_hash_set.rb
+++ w10d2/lib/p03_hash_set.rb
@@ -7,18 +7,37 @@ class HashSet
   end
 
   def insert(key)
+    if num_buckets == @count
+      resize!
+    end
+
+    if include?(key.hash) == false
+      @count += 1
+      self[key.hash] << key
+      return key
+    else
+      self[key.hash] << key
+    end
+    
   end
 
   def include?(key)
+    self[key.hash].include?(key)
   end
 
   def remove(key)
+    if include?(key)
+      @count -= 1
+      self[key.hash].delete(key)
+    end
+    nil
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
