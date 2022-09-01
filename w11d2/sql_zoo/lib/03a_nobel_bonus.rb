e8bc86be0b15700f268dc10687a36604b215ff3e
yippy <samueltylergreen@gmail.com>
2022-09-01 09:44:28 -0700

    Adds 03a solution

--- w11d2/sql_zoo/lib/03a_nobel_bonus.rb
+++ w11d2/sql_zoo/lib/03a_nobel_bonus.rb
@@ -11,5 +11,20 @@ require_relative './sqlzoo.rb'
 def physics_no_chemistry
   # In which years was the Physics prize awarded, but no Chemistry prize?
   execute(<<-SQL)
+    SELECT
+      DISTINCT a.yr
+    FROM
+      nobels AS a
+        WHERE
+          subject = 'Physics'
+        AND 
+          'Chemistry' NOT IN (
+          SELECT
+            subject 
+          FROM
+            nobels
+          WHERE 
+            a.yr = yr
+        )
   SQL
 end
