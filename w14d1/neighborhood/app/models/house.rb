20848275b1cb537cca0faceef61f771d05079071
yippy <samueltylergreen@gmail.com>
2022-10-15 14:32:43 -0700

    clean up

--- w14d1/neighborhood/app/models/house.rb
+++
@@ -1,9 +0,0 @@
-class House < ApplicationRecord
-
-  has_many(
-    :people,
-    class_name: 'Person',
-    foreign_key: :house_id,
-    primary_key: :id
-  )
-end
--- w14d1/neighborhood/db/migrate/20221013202021_add_constraint_to_houses.rb
+++
@@ -1,5 +0,0 @@
-class AddConstraintToHouses < ActiveRecord::Migration[7.0]
-  def change
-    change_column :houses, :address, :string, null: false
-  end
-end
